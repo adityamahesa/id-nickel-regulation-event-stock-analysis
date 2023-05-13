a49707accf08e2ac5d575d1b378fd449816b763b
Aditya A. Mahesa <aditya.mahesa@gmail.com>
2023-05-13 19:14:06 +0700

    feature: add correlations

--- util.py
+++ util.py
@@ -47,3 +47,26 @@ def prepare_top_10_tickers_per_sector(date: str) -> pd.DataFrame:
         dataframe_per_sector = dataframe_per_sector.iloc[0:10]
         gathered_market_values.extend(dataframe_per_sector.to_dict('records'))
     return pd.DataFrame(gathered_market_values)
+
+def generate_average_return_per_sector(data : pd.DataFrame, start_date : str, end_date : str = None) -> pd.DataFrame:
+    start_time = datetime.strptime(start_date, '%Y-%m-%d') - timedelta(days=1)
+    end_time = start_time + timedelta(days=2)
+    if end_date:
+        end_time = datetime.strptime(end_date, '%Y-%m-%d') + timedelta(days=1)
+
+    concat_object : dict[str, pd.Series] = {}
+    for sector in data_source.tickers.keys():
+        tickers_sectorized_df = data[data['sector'] == sector]
+        if len(tickers_sectorized_df) < 1:
+            continue
+        tickers_sectorized_series = tickers_sectorized_df['ticker']
+        tickers_sectorized = tickers_sectorized_series.to_list()
+        tickers_sectorized = prepare_tickers_suffix(tickers=tickers_sectorized)
+        prices_per_sector = yf.download(tickers=tickers_sectorized, start=start_time, end=end_time)
+        prices_per_sector_close = prices_per_sector['Close']
+        return_of_tickers = prices_per_sector_close.pct_change().iloc[1:]
+        sectoral_average_return = return_of_tickers.mean(axis=1)
+        concat_object[sector] = sectoral_average_return
+    average_return_per_sector = pd.concat(concat_object, axis=1)
+    
+    return average_return_per_sector
